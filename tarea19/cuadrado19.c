ff006bb9881b1d2ca6d4ae94b67a7a270f0f8272
BRIAN MENDOZA <31576390+brayan-mendoza@users.noreply.github.com>
2017-11-06 23:39:43 -0600

    tarea 19
    cuadrado con funcion

---
+++ tarea19/cuadrado19.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int figura(int a, int b){
+int x,y;
+	for(x=1; x<=a; x=x+1){
+	for(y=1; y<=b; y=y+1){
+		
+		printf("+ ");
+						}
+		printf("\n");
+		
+	
+						}
+	}
+int main() {
+	int a,b;
+	printf("dame la altura:\n");
+	scanf("%d", &a);
+	printf("dame la base\n");
+	scanf("%d", &b);
+	figura(a=a,b=b);
+	return 0;
+	
+}
